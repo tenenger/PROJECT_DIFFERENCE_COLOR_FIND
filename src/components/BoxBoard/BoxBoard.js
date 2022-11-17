0e6a46f41d341767941caf3261d8fe8e4d81c7fe
Lee Dong Gyu <tenenger7125@gmail.com>
2022-11-17 10:30:11 +0900

    refactor: style path, import style file variable

--- src/components/BoxBoard/BoxBoard.js
+++ src/components/BoxBoard/BoxBoard.js
@@ -1,4 +1,4 @@
-import Board_style from "../css/BoxBoard.module.css";
+import styles from "./BoxBoard.module.css";
 import BoxStyle from "./BoxStyle";
 import Setting from "./Setting";
 
@@ -23,13 +23,13 @@ function BoxBoard() {
   };
 
   return (
-    <div className={Board_style.outer}>
-      <div className={Board_style.content}>
+    <div className={styles.outer}>
+      <div className={styles.content}>
         <div>스테이지: {stage}</div>
         <div>남은시간: {remainTime}</div>
         <div>점수: {score}</div>
       </div>
-      <div className={Board_style.layout}>
+      <div className={styles.layout}>
         {/* 숫자만큼 태그를 반복하여 호출한다.(숫자를 배열로 변환) */}
         {[...Array(boxAmount)].map((_, index) =>
           index === diffBoxIdx ? (
